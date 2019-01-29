053dce1d3816bc82c3bf57ee047536fc2e1740a6
viktor_petukhov <viktor.s.petuhov@ya.ru>
2019-01-29 07:05:40 -0500

    Fixed Gadfly import

--- src/data_processing/plots.jl
+++ src/data_processing/plots.jl
@@ -1,5 +1,4 @@
 using ConcaveHull: concave_hull
-using Gadfly
 using Random
 
 import Colors
@@ -22,7 +21,7 @@ function plot_cell_borders_density(df_spatial::DataFrame, cell_labels::Array{Int
 end
 
 function plot_cell_borders_polygons(df_spatial::DataFrame, polygons::Array{Array{Float64, 2}, 1}, df_centers=nothing; point_size=2, color=:gene,
-                                    center_size::Real=50.0, polygon_line_width=1, size=(1000, 1000))
+                                    center_size::Real=50.0, polygon_line_width=1, size=(800, 600))
     if typeof(color) === Symbol
         color = df_spatial[color]
     end
