e4bad01c922b4efdf8b09cfcd142477768af3f92
viktor_petukhov <viktor.s.petuhov@ya.ru>
2019-07-22 15:01:55 -0400

    Convex hull tests instead of cli

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,23 @@
 using Baysor, Test
 
-@testset "cli" begin
-    @testset "Help" begin
-        redirect_stdout(open("/dev/null", "w")) do; Baysor.run_cli(["--help"]); end
-        @test 1 == 1
+@testset "Baysor" begin
+    @testset "utils" begin
+        @testset "convex_hull" begin
+            chull = Baysor.convex_hull([[0, 0], [1, 0], [0, 0], [1, 1], [0, 1]])
+            @test all(chull .== [0 0 1 1 0; 0 1 1 0 0])
+            @test Baysor.area(chull) ≈ 1.0
+
+            chull = Baysor.convex_hull([[0, 0], [1, 0], [0, 0], [1, 1], [0, 1], [0, 2], [2, 0]])
+            @test all(chull .== [0 0 2 0; 0 2 0 0])
+            @test Baysor.area(chull) ≈ 2.0
+
+            chull = Baysor.convex_hull([[0, 0], [1, 0], [0, 0], [1, 1], [0, 1], [0, 2], [2, 0], [2, 2]])
+            @test all(chull .== [0 0 2 2 0; 0 2 2 0 0])
+            @test Baysor.area(chull) ≈ 4.0
+        end
+
+        @testset "utils" begin
+            @test all([Baysor.interpolate_linear(x, 0.0, 1.0; y_start=0.0, y_end=1.0) ≈ x for x in range(0, 1.0, length=50)])
+        end
     end
 end
