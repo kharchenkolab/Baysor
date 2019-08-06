0109739d85e565cef6dee2cc4aa8f68db8baba7a
viktor_petukhov <viktor.s.petuhov@ya.ru>
2019-08-06 15:06:25 -0400

    Another constructor for ScaledInverseChisq

--- src/distributions/ScaledInverseChisq.jl
+++ src/distributions/ScaledInverseChisq.jl
@@ -1,8 +1,21 @@
 using Distributions
 
 struct ScaledInverseChisq <: Distributions.Distribution{Distributions.Univariate,Distributions.Continuous}
-    ν::Int
+    ν::Float64
     τ::Float64
+end
+
+# Specify ScaledInverseChisq from mean and variance:
+# μ = ντ² / (ν - 2)
+# σ² = 2ν²τ⁴ / ((ν - 2)² (ν - 4))
+# σ² = 2μ² / (ν - 4)
+# ν = 2μ² / σ² + 4
+# τ = √(μ (ν - 2) / ν)
+function ScaledInverseChisq(; μ::Float64, σ²::Float64)
+    ν = 2 * μ^2 / σ² + 4
+    τ = sqrt(μ * (ν - 2) / ν)
+    
+    return ScaledInverseChisq(ν, τ)
 end
 
 rand(d::ScaledInverseChisq, args::Vararg{Int}) = d.τ^2 .* d.ν ./ rand(Chisq(d.ν), args...)
