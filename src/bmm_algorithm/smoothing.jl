0bee4fed7f2b5c2429ff3303daa87a42ef1d0512
viktor_petukhov <viktor.s.petuhov@ya.ru>
2019-09-18 15:24:47 -0400

    Improved cell type prior (still doesn't work)

--- src/bmm_algorithm/smoothing.jl
+++ src/bmm_algorithm/smoothing.jl
@@ -2,9 +2,11 @@ using NearestNeighbors
 using DataFrames
 using ProgressMeter
 
+import Distances
 import Distributions
 import StatsBase
 import MultivariateStats
+import NearestNeighborDescent
 
 """
     estimate_gene_probs_given_single_transcript(cm, n_molecules_per_cell)
@@ -159,14 +161,18 @@ function extract_gene_matrix_from_distributions2(components::Array{Component, 1}
         end
     end
 
-    return hcat(counts_per_cell...);
+    res_mat = hcat(counts_per_cell...);
+    res_mat[:, [c.n_samples == 0 for c in components]] .= 0
+
+    return res_mat;
 end
 
 function knn_by_expression2(count_matrix::Array{T, 2} where T <: Real,
                             n_molecules_per_cell::Array{Int, 1}=round.(Int, vec(sum(count_matrix, dims=1)));
                             k::Int=15, min_molecules_per_cell::Int=10, n_prin_comps::Int=0, refine::Bool=true,
-                            refine_mult::Real = 2)::Array{Array{Int, 1}, 1}
-    count_matrix_norm = count_matrix ./ sum(count_matrix, dims=1)
+                            refine_mult::Real = 2, use_correlations::Bool=false)::Array{Array{Int, 1}, 1}
+    count_matrix_norm = count_matrix ./ max.(sum(count_matrix, dims=1), 1e-50)
+    # count_matrix_norm = log2.(1e-6 .+ count_matrix ./ sum(count_matrix, dims=1))
     neighborhood_matrix = count_matrix_norm
 
     real_cell_inds = findall(n_molecules_per_cell .>= min_molecules_per_cell);
@@ -186,14 +192,86 @@ function knn_by_expression2(count_matrix::Array{T, 2} where T <: Real,
         k = length(real_cell_inds)
     end
 
-    kd_tree = KDTree(neighborhood_matrix[:, real_cell_inds]);
-    neighb_inds = [ids[2:end] for ids in knn(kd_tree, neighborhood_matrix, (refine ? refine_mult * k : k) + 1, true)[1]]
-
-    if refine
-        return [refine_knn_by_expression(neighborhood_matrix, real_cell_inds[inds], k=k) for inds in neighb_inds]
+    neighb_inds = Int[]
+    k0 = (refine ? refine_mult * k : k)
+    if use_correlations
+        p_dists = Distances.pairwise(Distances.CosineDist(), neighborhood_matrix, neighborhood_matrix[:, real_cell_inds], dims=2);
+        res = [real_cell_inds[inds] for inds in eachrow(mapslices(sortperm, p_dists, dims=2)[:,1:(k0 + 1)])]
+        neighb_inds = [v[v .!= i][1:k] for (i, v) in enumerate(res)]
+    else
+        kd_tree = KDTree(neighborhood_matrix[:, real_cell_inds]);
+        neighb_inds = [real_cell_inds[ids[2:end]] for ids in knn(kd_tree, neighborhood_matrix, k0 + 1, true)[1]]
     end
 
-    return [real_cell_inds[inds] for inds in neighb_inds]
+    if !refine
+        return neighb_inds
+    end
+
+    return refine_knn_by_expression.(Ref(neighborhood_matrix), neighb_inds, k=k)
+end
+
+"""
+    This function is very sensitive to proper selection of metric and reduction for NN search
+"""
+function knn_by_expression_robust(count_matrix::Array{T, 2} where T <: Real,
+                                  n_molecules_per_cell::Array{Int, 1}=round.(Int, vec(sum(count_matrix, dims=1)));
+                                  k::Int=10, min_molecules_per_cell::Int=10, distance::Union{T2, Nothing} where T2 <: Distances.SemiMetric = nothing,
+                                  n_prin_comps::Union{Int, Nothing}=nothing, approximate::Bool=false, kwargs...) #::Array{Array{Int, 1}, 1}
+    neighborhood_matrix = count_matrix ./ max.(sum(count_matrix, dims=1), 1e-50)
+    real_cell_inds = findall(n_molecules_per_cell .>= min_molecules_per_cell);
+
+    if maximum(n_molecules_per_cell) < min_molecules_per_cell
+        @warn "No cells pass min_molecules threshold ($min_molecules_per_cell). Resetting it to 1"
+        min_molecules_per_cell = 1
+    end
+
+    if length(real_cell_inds) < k
+        @warn "Number of large cells ($(length(real_cell_inds))) is lower than the requested number of nearest neighbors ($k)"
+        k = length(real_cell_inds)
+    end
+
+    if n_prin_comps === nothing
+        n_prin_comps = max(min(ceil(Int, 0.75 * median(sum(count_matrix .> 0, dims=1))), 100), 3)
+    end
+
+    if n_prin_comps > 0
+        pca = MultivariateStats.fit(MultivariateStats.PCA, neighborhood_matrix[:, real_cell_inds]; maxoutdim=n_prin_comps);
+        neighborhood_matrix = MultivariateStats.transform(pca, neighborhood_matrix)
+    end
+
+    if distance === nothing
+        distance = (size(neighborhood_matrix, 1) > 20) ? Distances.CosineDist() : Distances.Euclidean()
+    end
+
+    if approximate
+        graph = NearestNeighborDescent.DescentGraph(neighborhood_matrix[:, real_cell_inds], k, distance; kwargs...);
+        g_ids, g_dists = NearestNeighborDescent.search(graph, neighborhood_matrix, k);
+        nn_per_cell = [real_cell_inds[ids[sortperm(dists)]] for (ids, dists) in zip(eachcol(g_ids), eachcol(g_dists))];
+    else
+        if typeof(distance) <: Distances.Metric
+            kd_tree = KDTree(neighborhood_matrix[:, real_cell_inds]);
+            nn_per_cell = [real_cell_inds[ids] for ids in knn(kd_tree, neighborhood_matrix, k + 1, true)[1]]
+        else
+            @warn "Using bruteforce NN computation. It can take long..."
+            p_dists = Distances.pairwise(distance, neighborhood_matrix, neighborhood_matrix[:, real_cell_inds], dims=2);
+            nn_per_cell = [real_cell_inds[inds] for inds in eachrow(mapslices(sortperm, p_dists, dims=2)[:,1:(k + 1)])]
+        end
+
+        nn_per_cell = [v[v .!= i][1:k] for (i, v) in enumerate(nn_per_cell)]
+    end
+
+    # return nn_per_cell
+
+    nn_variance_per_cell = [quantile(vec(Distances.pairwise(distance, neighborhood_matrix[:, ids], dims=2)), 0.95) for ids in nn_per_cell];
+    non_doublet_cells = unique([ids[findmin(nn_variance_per_cell[ids])[2]] for ids in nn_per_cell]);
+    is_cell_non_doublet = falses(length(nn_variance_per_cell));
+    is_cell_non_doublet[non_doublet_cells] .= true;
+
+    non_doublet_nn_per_cell = [ids[findfirst(is_cell_non_doublet[ids])] for ids in nn_per_cell];
+    # return nn_per_cell #, is_cell_non_doublet
+    # return [ids[is_cell_non_doublet[ids]] for ids in nn_per_cell];
+    # return [[ids[findfirst(is_cell_non_doublet[ids])]] for ids in nn_per_cell]
+    return nn_per_cell[non_doublet_nn_per_cell] #, nn_per_cell, [ids[is_cell_non_doublet[ids]] for ids in nn_per_cell];
 end
 
 function refine_knn_by_expression(neighborhood_matrix::Matrix{Float64}, neighb_inds::Vector{Int}; k::Int, max_iter::Int = 100)
