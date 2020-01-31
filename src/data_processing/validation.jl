32b510b5dc6a008b5ea084bbfdfae04174bb86c7
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-01-31 07:56:07 -0500

    Fixed mixing scores

--- src/data_processing/validation.jl
+++ src/data_processing/validation.jl
@@ -3,8 +3,8 @@ import Plots
 
 using Statistics
 
-center_dists(pos_data::Matrix{Float64}, ids_per_clust::Array{Vector{Int}, 1}) =
-    sum(diff(hcat([mean(pos_data[:, ids], dims=2) for ids in ids_per_clust]...), dims=2) .^ 2)
+center_dists(pos_data::Matrix{Float64}, ids_per_clust1::Vector{Int}, ids_per_clust2::Vector{Int}) =
+    sum(diff(hcat([mean(pos_data[:, ids], dims=2) for ids in [ids_per_clust1, ids_per_clust2]]...), dims=2) .^ 2)
 
 function permutation_mixing_score(cell_pos_data::Matrix{Float64}, cell_mol_clusts::Vector{Int}; n_permutations::Int=1000, threshold::Float64=0.05)
     ids_per_clust = split_ids(cell_mol_clusts)
@@ -20,12 +20,14 @@ function permutation_mixing_score(cell_pos_data::Matrix{Float64}, cell_mol_clust
             continue
         end
 
-        obs_dist = center_dists(cell_pos_data, ids_per_clust)
+        obs_dist = center_dists(cell_pos_data, ids_per_clust[1], wrong_clust_ids)
 
         dists = Float64[]
         for i in 1:n_permutations
-            s_ids = split_ids(Random.shuffle(cell_mol_clusts))
-            push!(dists, center_dists(cell_pos_data, s_ids[sortperm(length.(s_ids), rev=true)[1:2]]))
+            labels_shuffled = Random.shuffle(cell_mol_clusts)
+            push!(dists, center_dists(cell_pos_data,
+                findall(labels_shuffled .== cell_mol_clusts[ids_per_clust[1][1]]),
+                findall(labels_shuffled .== cell_mol_clusts[wrong_clust_ids[1]])))
         end
 
         if mean(dists .>= obs_dist) < threshold
@@ -36,10 +38,91 @@ function permutation_mixing_score(cell_pos_data::Matrix{Float64}, cell_mol_clust
     return n_wrong_mols / length(cell_mol_clusts)
 end
 
-function mixing_score_per_cell(position_data::Matrix{Float64}, cell_assignment::Vector{Int}, clust_per_mol::Vector{Int}; min_mols_per_cell::Int)
+function mixing_score_per_cell(position_data::Matrix{Float64}, cell_assignment::Vector{Int}, clust_per_mol::Vector{Int}; min_mols_per_cell::Int, kwargs...)
     ids_per_cell = split_ids(cell_assignment .+ 1)[2:end]
     real_cell_ids = findall(length.(ids_per_cell) .>= min_mols_per_cell)
-    wrong_frac_per_cell = [permutation_mixing_score(position_data[:, ids], clust_per_mol[ids]) for ids in ids_per_cell[real_cell_ids]];
+    wrong_frac_per_cell = [permutation_mixing_score(position_data[:, ids], clust_per_mol[ids]; kwargs...) for ids in ids_per_cell[real_cell_ids]];
 
     return wrong_frac_per_cell, length.(ids_per_cell[real_cell_ids])
+end
+
+function mixing_score_per_molecule(position_data::Matrix{Float64}, cell_assignment::Vector{Int}, clust_per_mol::Vector{Int}; min_mols_per_cell::Int, kwargs...)
+    ids_per_cell = split_ids(cell_assignment .+ 1)[2:end]
+    real_cell_ids = findall(length.(ids_per_cell) .>= min_mols_per_cell)
+    wrong_frac_per_cell = [permutation_mixing_score(position_data[:, ids], clust_per_mol[ids]; kwargs...) for ids in ids_per_cell[real_cell_ids]];
+    wrong_frac_per_mol = zeros(length(cell_assignment))
+    for (frac, ids) in zip(wrong_frac_per_cell, ids_per_cell[real_cell_ids])
+        wrong_frac_per_mol[ids] .= frac
+    end
+
+    return wrong_frac_per_mol
+end
+
+function plot_mixing_scores(cell_score_baysor::Vector{Float64}, n_mols_per_cell_baysor::Vector{Int},
+                            cell_score_paper::Vector{Float64}, n_mols_per_cell_paper::Vector{Int},
+                            cell_score_random::Union{Vector{Float64}, Nothing}, n_mols_per_cell_random::Union{Vector{Int}, Nothing}=nothing;
+                            min_mols_per_cell::Int=0, layout=(2, 2), legend=:best, size1=(400, 300), size2=(800, 600), kwargs...)
+
+    cell_score_baysor = cell_score_baysor[n_mols_per_cell_baysor .>= min_mols_per_cell]
+    n_mols_per_cell_baysor = n_mols_per_cell_baysor[n_mols_per_cell_baysor .>= min_mols_per_cell]
+    cell_score_paper = cell_score_paper[n_mols_per_cell_paper .>= min_mols_per_cell]
+    n_mols_per_cell_paper = n_mols_per_cell_paper[n_mols_per_cell_paper .>= min_mols_per_cell]
+
+    if cell_score_random !== nothing
+        cell_score_random = cell_score_random[n_mols_per_cell_random .>= min_mols_per_cell]
+        n_mols_per_cell_random = n_mols_per_cell_random[n_mols_per_cell_random .>= min_mols_per_cell]
+    end
+
+    p0 = Plots.scatter(n_mols_per_cell_baysor, cell_score_baysor, xlabel="Cell size", ylabel="Mixing score", label="Baysor",
+        markersize=3, size=size1, legend=legend)
+    p0 = Plots.scatter!(n_mols_per_cell_paper, cell_score_paper, label="Paper", markersize=3)
+
+    if cell_score_random !== nothing
+        p0 = Plots.scatter!(n_mols_per_cell_random, cell_score_random, label="Random", markersize=3)
+    end
+
+    t_xvals = sort(unique(n_mols_per_cell_paper))
+    p1 = Plots.scatter(t_xvals, [mean((cell_score_paper .* n_mols_per_cell_paper)[n_mols_per_cell_paper .<= v]) for v in t_xvals], label="Paper",
+        legend=legend, bg_legend=Colors.RGBA(1, 1, 1, 0.5), fg_legend=Colors.RGBA(0, 0, 0, 0.0), xlabel="Max cell size", ylabel="Mean number of 'wrong'\nmolecules per cell")
+
+    t_xvals = sort(unique(n_mols_per_cell_baysor))
+    p1 = Plots.scatter!(t_xvals, [mean((cell_score_baysor .* n_mols_per_cell_baysor)[n_mols_per_cell_baysor .<= v]) for v in t_xvals], label="Baysor")
+
+    if cell_score_random !== nothing
+        t_xvals = sort(unique(n_mols_per_cell_random))
+        p1 = Plots.scatter!(t_xvals, [mean((cell_score_random .* n_mols_per_cell_random)[n_mols_per_cell_random .<= v]) for v in t_xvals], label="Random")
+    end
+
+    t_xvals = sort(unique(n_mols_per_cell_paper))
+    p2 = Plots.scatter(t_xvals, [mean(cell_score_paper[n_mols_per_cell_paper .<= v]) for v in t_xvals], label="Paper",
+        legend=:none, xlabel="Max cell size", ylabel="Contamination")
+
+    t_xvals = sort(unique(n_mols_per_cell_baysor))
+    p2 = Plots.scatter!(t_xvals, [mean(cell_score_baysor[n_mols_per_cell_baysor .<= v]) for v in t_xvals], label="Baysor")
+
+    t_xvals = sort(unique(n_mols_per_cell_paper))
+    p3 = Plots.scatter(t_xvals, [mean((cell_score_paper .* n_mols_per_cell_paper)[n_mols_per_cell_paper .>= v]) for v in t_xvals], label="Paper",
+        legend=:none, xlabel="Min cell size", ylabel="Mean number of 'wrong'\nmolecules per cell")
+
+    t_xvals = sort(unique(n_mols_per_cell_baysor))
+    p3 = Plots.scatter!(t_xvals, [mean((cell_score_baysor .* n_mols_per_cell_baysor)[n_mols_per_cell_baysor .>= v]) for v in t_xvals], label="Baysor")
+
+    if cell_score_random !== nothing
+        t_xvals = sort(unique(n_mols_per_cell_random))
+        p3 = Plots.scatter!(t_xvals, [mean((cell_score_random .* n_mols_per_cell_random)[n_mols_per_cell_random .>= v]) for v in t_xvals], label="Random")
+    end
+
+    t_xvals = sort(unique(n_mols_per_cell_paper))
+    p4 = Plots.scatter(t_xvals, [mean(cell_score_paper[n_mols_per_cell_paper .>= v]) for v in t_xvals], label="Paper",
+        legend=:none, xlabel="Min cell size", ylabel="Contamination")
+
+    t_xvals = sort(unique(n_mols_per_cell_baysor))
+    p4 = Plots.scatter!(t_xvals, [mean(cell_score_baysor[n_mols_per_cell_baysor .>= v]) for v in t_xvals], label="Baysor")
+
+    if cell_score_random !== nothing
+        t_xvals = sort(unique(n_mols_per_cell_random))
+        p4 = Plots.scatter!(t_xvals, [mean(cell_score_random[n_mols_per_cell_random .>= v]) for v in t_xvals], label="Random")
+    end
+
+    return p0, Plots.plot(p1, p2, p3, p4; layout=layout, size=size2, kwargs...)
 end
