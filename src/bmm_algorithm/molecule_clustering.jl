5bfcd8c71359a8fe24a740bb21b1b9bbd2122469
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-03-03 22:31:42 -0500

    backup mol functions

--- src/bmm_algorithm/molecule_clustering.jl
+++ src/bmm_algorithm/molecule_clustering.jl
@@ -14,6 +14,25 @@ function maximize_mols!(cell_type_exprs::Matrix{Float64}, genes::Vector{Int}, as
     cell_type_exprs ./= sum(cell_type_exprs, dims=2)
 end
 
+# function expect_mols!(assignment::Vector{Int}, cell_type_exprs::Matrix{Float64}, genes::Vector{Int},
+#         adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}}) # Add confidence?
+#     component_weights = Dict{Int, Float64}();
+#     adj_classes = Int[]
+#     adj_weights = Float64[]
+#     denses = Float64[]
+#     for i in 1:length(genes)
+#         gene = genes[i]
+#         adjacent_component_weights!(adj_weights, adj_classes, component_weights, assignment, adjacent_points[i], adjacent_weights[i])
+
+#         empty!(denses)
+#         for j in eachindex(adj_weights)
+#             push!(denses, adj_weights[j] * cell_type_exprs[adj_classes[j], gene])
+#         end
+
+#         assignment[i] = fsample(adj_classes, denses)
+#     end
+# end
+
 function expect_mols!(assignment::Vector{Int}, cell_type_exprs::Matrix{Float64}, genes::Vector{Int},
         adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}}; new_prob::Float64=0.05) # Add confidence?
     denses = zeros(size(cell_type_exprs, 1))
@@ -38,6 +57,7 @@ function expect_mols!(assignment::Vector{Int}, cell_type_exprs::Matrix{Float64},
     end
 end
 
+# TODO: rename
 function optimize_mols(genes::Vector{Int}, adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}};
         k::Int, n_iters::Int=100, history_depth::Int=50, new_prob::Float64=0.001)
     cell_type_exprs = copy(hcat([rand(maximum(genes)) for i in 1:k]...)');
@@ -65,10 +85,10 @@ function build_molecule_graph(df_spatial::DataFrame)
     return convert_edge_list_to_adj_list(edge_list, adjacent_weights; n_verts=size(df_spatial, 1));
 end
 
-function reorder_clusters(cell_type_exprs::Matrix{Float64}, assignment::Vector{Int})
-    cl_order = sortperm(count_array(assignment), rev=true);
-    return cell_type_exprs[cl_order,:], get.(Ref(Dict(Pair.(cl_order, 1:10))), assignment, 0)
-end
+# function reorder_clusters(cell_type_exprs::Matrix{Float64}, assignment::Vector{Int})
+#     cl_order = sortperm(count_array(assignment), rev=true);
+#     return cell_type_exprs[cl_order,:], get.(Ref(Dict(Pair.(cl_order, 1:10))), assignment, 0)
+# end
 
-reorder_clusters(assignment::Vector{Int}) =
-    get.(Ref(Dict(Pair.(sortperm(count_array(assignment), rev=true), 1:10))), assignment, 0)
+# reorder_clusters(assignment::Vector{Int}) =
+#     get.(Ref(Dict(Pair.(sortperm(count_array(assignment), rev=true), 1:10))), assignment, 0)
