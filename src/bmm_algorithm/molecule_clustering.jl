c00a1b6c7569b01e64365a6d8d96d6c18e96c833
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-03-04 17:58:06 -0500

    Use exp for edge weights. Edge normalization for [0; 1] scale

--- src/bmm_algorithm/molecule_clustering.jl
+++ src/bmm_algorithm/molecule_clustering.jl
@@ -14,43 +14,28 @@ function maximize_mols!(cell_type_exprs::Matrix{Float64}, genes::Vector{Int}, as
     cell_type_exprs ./= sum(cell_type_exprs, dims=2)
 end
 
-# function expect_mols!(assignment::Vector{Int}, cell_type_exprs::Matrix{Float64}, genes::Vector{Int},
-#         adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}}) # Add confidence?
-#     component_weights = Dict{Int, Float64}();
-#     adj_classes = Int[]
-#     adj_weights = Float64[]
-#     denses = Float64[]
-#     for i in 1:length(genes)
-#         gene = genes[i]
-#         adjacent_component_weights!(adj_weights, adj_classes, component_weights, assignment, adjacent_points[i], adjacent_weights[i])
-
-#         empty!(denses)
-#         for j in eachindex(adj_weights)
-#             push!(denses, adj_weights[j] * cell_type_exprs[adj_classes[j], gene])
-#         end
-
-#         assignment[i] = fsample(adj_classes, denses)
-#     end
-# end
-
 function expect_mols!(assignment::Vector{Int}, cell_type_exprs::Matrix{Float64}, genes::Vector{Int},
-        adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}}; new_prob::Float64=0.05) # Add confidence?
+        adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}}; new_prob::Float64=0.001,
+        mrf_prior_weight::Float64=1.0) # Add confidence?
     denses = zeros(size(cell_type_exprs, 1))
     for i in 1:length(genes)
         gene = genes[i]
         cur_weights = adjacent_weights[i]
         cur_points = adjacent_points[i]
 
-        sum_dens = sum(cur_weights)
         denses .= 0.0
 
-        if rand() < new_prob
+        if rand() < new_prob # allowing to link new component improves global convergence
             assignment[i] = fsample(cell_type_exprs[:, gene])
         else
-            @inbounds @simd for j in 1:length(cur_weights)
-                denses[assignment[cur_points[j]]] += cur_weights[j]
+            for j in 1:length(cur_points)
+                ca = assignment[cur_points[j]]
+                denses[ca] = cell_type_exprs[ca, gene]
             end
-            denses .*= cell_type_exprs[:, gene]
+
+            for j in 1:length(cur_weights)
+                denses[assignment[cur_points[j]]] *= exp(cur_weights[j] * mrf_prior_weight)
+            end
 
             assignment[i] = fsample(denses)
         end
@@ -59,13 +44,14 @@ end
 
 # TODO: rename
 function optimize_mols(genes::Vector{Int}, adjacent_points::Vector{Vector{Int}}, adjacent_weights::Vector{Vector{Float64}};
-        k::Int, n_iters::Int=100, history_depth::Int=50, new_prob::Float64=0.001)
+        k::Int, n_iters::Int=100, history_depth::Int=50, new_prob::Float64=0.001, mrf_prior_weight::Float64=1.0)
     cell_type_exprs = copy(hcat([rand(maximum(genes)) for i in 1:k]...)');
     assignment = rand(1:k, length(genes));
     assignment_history = Vector{Int}[]
     maximize_mols!(cell_type_exprs, genes, assignment)
     @showprogress for i in 1:n_iters
-        expect_mols!(assignment, cell_type_exprs, genes, adjacent_points, adjacent_weights, new_prob=new_prob)
+        expect_mols!(assignment, cell_type_exprs, genes, adjacent_points, adjacent_weights, new_prob=new_prob,
+            mrf_prior_weight=mrf_prior_weight)
         maximize_mols!(cell_type_exprs, genes, assignment)
         if (n_iters - i) >= history_depth
             push!(assignment_history, deepcopy(assignment))
@@ -77,18 +63,22 @@ function optimize_mols(genes::Vector{Int}, adjacent_points::Vector{Vector{Int}},
     return cell_type_exprs, assignment_cons, assignment_history
 end
 
-function build_molecule_graph(df_spatial::DataFrame)
-    edge_list, adjacent_dists = adjacency_list(df_spatial);
+function build_molecule_graph(df_spatial::DataFrame; kwargs...)
+    edge_list, adjacent_dists = adjacency_list(df_spatial; kwargs...);
+
     real_edge_length = quantile(adjacent_dists, 0.3);
-    adjacent_weights = 1 ./ max.(adjacent_dists, real_edge_length);
+    adjacent_weights = real_edge_length ./ max.(adjacent_dists, real_edge_length);
 
     return convert_edge_list_to_adj_list(edge_list, adjacent_weights; n_verts=size(df_spatial, 1));
 end
 
-# function reorder_clusters(cell_type_exprs::Matrix{Float64}, assignment::Vector{Int})
-#     cl_order = sortperm(count_array(assignment), rev=true);
-#     return cell_type_exprs[cl_order,:], get.(Ref(Dict(Pair.(cl_order, 1:10))), assignment, 0)
-# end
+# function build_molecule_knn_graph(df_spatial::DataFrame)
+#     edge_list, adjacent_dists = knn(KDTree(position_data(df_spatial)), position_data(df_spatial), 6, true)
+#     edge_list = [el[2:end] for el in edge_list]
+#     adjacent_dists = [ad[2:end] for ad in adjacent_dists]
 
-# reorder_clusters(assignment::Vector{Int}) =
-#     get.(Ref(Dict(Pair.(sortperm(count_array(assignment), rev=true), 1:10))), assignment, 0)
+#     real_edge_length = quantile(vcat(adjacent_dists...), 0.3);
+#     adjacent_weights = [real_edge_length ./ max.(ad, real_edge_length) for ad in adjacent_dists];
+
+#     return edge_list, adjacent_weights
+# end
