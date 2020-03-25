bfcfe6565b3a1fc00e883a8c643d78dfda58325a
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2020-03-25 12:56:03 -0400

    Improved loading spatial df

--- src/utils/spatial.jl
+++ src/utils/spatial.jl
@@ -4,30 +4,30 @@ using DataFrames
 function read_spatial_df(data_path; x_col::Symbol=:x, y_col::Symbol=:y, gene_col::Union{Symbol, Nothing}=:gene, filter_cols::Bool=false)
     df_spatial = CSV.read(data_path) |> DataFrame;
 
-    if (:x in names(df_spatial)) & (x_col != :x)
-        if :x_reserved in names(df_spatial)
-            DataFrames.select!(df_spatial, DataFrames.Not(:x_reserved))
+    for (cn, co) in zip((:x, :y, :gene), (x_col, y_col, gene_col))
+        if co == nothing
+            continue
         end
-        DataFrames.rename!(df_spatial, :x => :x_reserved);
-    end
 
-    if (:y in names(df_spatial)) & (y_col != :y)
-        if :y_reserved in names(df_spatial)
-            DataFrames.select!(df_spatial, DataFrames.Not(:y_reserved))
+        if (cn in names(df_spatial)) & (cn != co)
+            cr = Symbol(String(cn) * "_reserved")
+            if cr in names(df_spatial)
+                DataFrames.select!(df_spatial, DataFrames.Not(cr))
+            end
+            DataFrames.rename!(df_spatial, cn => cr);
         end
-        DataFrames.rename!(df_spatial, :y => :y_reserved);
+
+        DataFrames.rename!(df_spatial, co => cn);
     end
 
     if gene_col === nothing
         if filter_cols
-            df_spatial = df_spatial[:, [x_col, y_col]]
+            df_spatial = df_spatial[:, [:x, :y]]
         end
-        DataFrames.rename!(df_spatial, x_col => :x, y_col => :y);
     else
         if filter_cols
-            df_spatial = df_spatial[:, [x_col, y_col, gene_col]]
+            df_spatial = df_spatial[:, [:x, :y, :gene]]
         end
-        DataFrames.rename!(df_spatial, x_col => :x, y_col => :y, gene_col => :gene);
         df_spatial[!, :gene] = ["$g" for g in df_spatial.gene]
     end
 
