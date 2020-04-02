1e603d90c6bf9a704cee5072c1a0f96a763cdd69
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2020-04-01 22:05:02 -0400

    Chenged default parameters of UMAP embedding

--- src/data_processing/umap_wrappers.jl
+++ src/data_processing/umap_wrappers.jl
@@ -15,14 +15,15 @@ struct UmapFit
     nn_interpolate::Int;
 end
 
-function fit(::Type{UmapFit}, x::Array{Float64, 2}; n_pcs::Int=15, n_components::Int=2, nn_interpolate::Int=5, kwargs...)::UmapFit # , metric::SemiMetric=Euclidean()
+function fit(::Type{UmapFit}, x::Array{Float64, 2}; n_pcs::Int=15, n_components::Int=2, nn_interpolate::Int=5,
+        spread=2.0, min_dist=0.1, kwargs...)::UmapFit # , metric::SemiMetric=Euclidean()
     pca = fit(MultivariateStats.PCA, x, maxoutdim=n_pcs)
     x_pc = transform(pca, x)
     # nn_tree = DescentGraph(x_pc, nn_interpolate, metric)
     # indices, distances = search(transformation.nn_tree, x_pc, transformation.nn_interpolate)
     # embedding = umap(x_pc, n_components; metric=metric, kwargs...)
     nn_tree = KDTree(x_pc)
-    embedding = umap(x_pc, n_components; kwargs...)
+    embedding = umap(x_pc, n_components; spread=spread, min_dist=min_dist, kwargs...)
 
     return UmapFit(embedding, nn_tree, pca, nn_interpolate)
 end
@@ -31,9 +32,6 @@ function transform(transformation::UmapFit, x::Array{Float64, 2}; dist_offset::F
     x_pc = transform(transformation.pca_transform, x)
     # indices, distances = search(transformation.nn_tree, x_pc, transformation.nn_interpolate)
     indices, distances = knn(transformation.nn_tree, x_pc, transformation.nn_interpolate)
-
-    res = [sum(transformation.embedding[:,ids]' ./ (dists .+ dist_offset), dims=1) ./ sum(1 ./ (dists .+ dist_offset))
-        for (ids, dists) in zip(indices, distances)]
-
-    return copy(vcat(res...)')
+    res = [mapslices(x -> wmean(x, 1 ./ (dists .+ dist_offset)), transformation.embedding[:, ids], dims=2) for (ids, dists) in zip(indices, distances)]
+    return hcat(res...)
 end
