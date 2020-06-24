be2a05a18a2cdad6a1578bf998d124fde040dfe8
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2020-06-24 10:49:56 +0200

    Tests for initialization

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,6 +46,21 @@ B = Baysor
 
     @testset "data_processing" begin
         @testset "initialization" begin
+            df = DataFrame(:x => rand(1000), :y => rand(1000), :gene => rand(1:10, 1000))
+
+            for i in 1:10
+                bm_data_arr = B.initial_distribution_arr(df, n_frames=i, scale=6.0, min_molecules_per_cell=30);
+                @test length(bm_data_arr) <= i
+            end
+
+            df[!, :cluster] = rand(1:5, 1000)
+            df[!, :prior_segmentation] = rand(1:100, 1000)
+            bm_data = B.initial_distribution_arr(df, n_frames=1, scale=6.0, min_molecules_per_cell=30)[1];
+            @test all(bm_data.cluster_per_molecule .== df.cluster)
+            @test all(bm_data.segment_per_molecule .== df.prior_segmentation)
+        end
+
+        @testset "parse_parameters" begin
             @test B.parse_scale_std("23.55%", 121.0) ≈ 121.0 * 0.2355
             @test B.parse_scale_std(33.87, 121.0) ≈ 33.87
             @test B.parse_scale_std(nothing, 121.0) ≈ 121.0 * 0.25
