240b1f975c78b9af13f5073a161b917a773cc207
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2020-07-14 12:13:46 +0200

    Small refactoring

--- src/bmm_algorithm/bmm_algorithm.jl
+++ src/bmm_algorithm/bmm_algorithm.jl
@@ -312,7 +312,7 @@ track_progress!(progress::Progress) = next!(progress)
 function bmm!(data::BmmData; min_molecules_per_cell::Int, n_iters::Int=1000, log_step::Int=4, verbose=true,
               new_component_frac::Float64=0.05, new_component_weight::Float64=0.3,
               assignment_history_depth::Int=0, trace_components::Bool=false, progress::Union{Progress, RemoteChannel, Nothing}=nothing,
-              component_split_step::Int=3) where D <: Distances.SemiMetric
+              component_split_step::Int=3)
     time_start = now()
 
     if verbose
@@ -344,9 +344,6 @@ function bmm!(data::BmmData; min_molecules_per_cell::Int, n_iters::Int=1000, log
         end
 
         maximize!(data, min_molecules_per_cell)
-
-        trace_n_components!(data, min_molecules_per_cell);
-
         drop_unused_components!(data)
 
         it_num = i
@@ -354,6 +351,7 @@ function bmm!(data::BmmData; min_molecules_per_cell::Int, n_iters::Int=1000, log
             log_em_state(data, it_num, time_start)
         end
 
+        trace_n_components!(data, min_molecules_per_cell);
         trace_assignment_history!(data, assignment_history_depth; use_guids=!trace_components)
         if trace_components
             trace_component_history!(data)
