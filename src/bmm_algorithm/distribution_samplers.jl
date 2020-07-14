e88da8287778ef4cf4da3e1bf55df9136f258f78
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2020-07-14 13:52:31 +0200

    Fixed distribution sampling

--- src/bmm_algorithm/distribution_samplers.jl
+++ src/bmm_algorithm/distribution_samplers.jl
@@ -1,14 +1,14 @@
 using Random
 
-function sample_distribution!(data::BmmData, shape_prior::ShapePrior)::MvNormalF
-    μ = sample_center!(data) # TODO: should we stor it as non-static arrays from the beginning?
+function sample_position_params!(data::BmmData, shape_prior::ShapePrior)::MvNormalF
+    μ = sample_center!(data) # TODO: should we store it as non-static arrays from the beginning?
     Σ = Array(Diagonal(shuffle(sample_var(shape_prior))))
 
     return MvNormalF(μ, Σ)
 end
 
 function sample_center!(data::BmmData; cache_size::Int=10000)
-    if length(data.center_sample_cache) == 0
+    if length(data.center_sample_cache) == 0 # otherwise, sampling takes too long
         data.center_sample_cache = sample(1:size(data.x, 1), Weights(confidence(data)), cache_size)
     end
 
@@ -16,15 +16,16 @@ function sample_center!(data::BmmData; cache_size::Int=10000)
 end
 
 function sample_composition_params(data::BmmData)
-    # TODO: fix this
+    # TODO: this doesn't work well on small samples.
+    # A simple workaround could be to further sample from the smoothed probabilities after sampling an existing distribution.
     gene_counts = sample(data.components).composition_params.counts
     return CategoricalSmoothed(gene_counts, smooth=data.distribution_sampler.composition_params.smooth, sum_counts=sum(gene_counts));
 end
 
 function sample_distribution!(data::BmmData; guid::Int)
     sampler = data.distribution_sampler
-    position_params = sample_distribution!(data, sampler.shape_prior);
+    position_params = sample_position_params!(data, sampler.shape_prior);
     composition_params = sample_composition_params(data);
 
-    return Component(position_params, composition_params; can_be_dropped=true, shape_prior=deepcopy(sampler.shape_prior), guid=guid);
+    return Component(position_params, composition_params; shape_prior=deepcopy(sampler.shape_prior), guid=guid);
 end
