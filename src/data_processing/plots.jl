f694fed30f7b7c5b01f9c2a12f09c539e315dcef
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2021-04-23 13:27:20 +0200

    Moved clustermap to BaysorAnalysis

--- src/data_processing/plots.jl
+++ src/data_processing/plots.jl
@@ -136,34 +136,8 @@ function plot_expression_vectors(vecs...; gene_names::Vector{String}, min_expr_f
     if xticks
         Plots.xticks!(1:length(gene_names), gene_names, xrotation=xrotation, xgrid=false)
     end
+    
     return p
-end
-
-function clustermap(mtx::T where T <: AbstractMatrix{Float64}, gene_names::Vector{String}; gene_ord::Union{<:AbstractVector{<:Integer}, Bool}=true,
-        cell_ord::Union{<:AbstractVector{<:Integer},Bool}=true, diag_genes::Bool=false, kwargs...)
-    if typeof(cell_ord) === Bool
-        if cell_ord
-            cell_dists = 1 .- cor(mtx);
-            cell_ord = Clustering.hclust(cell_dists, linkage=:ward).order;
-        else
-            cell_ord = 1:size(mtx, 2)
-        end
-    end
-
-    if typeof(gene_ord) === Bool
-        if gene_ord
-            if diag_genes
-                gene_ord = sortperm(vec(mapslices(x -> findmax(x)[2], mtx[:, cell_ord], dims=2)), rev=true);
-            else
-                gene_dists = 1 .- cor(mtx');
-                gene_ord = Clustering.hclust(gene_dists, linkage=:ward).order;
-            end
-        else
-            gene_ord = 1:size(mtx, 1)
-        end
-    end
-
-    Plots.heatmap(mtx[gene_ord, cell_ord]; yticks=(1:length(gene_names), gene_names[gene_ord]), kwargs...), cell_ord, gene_ord
 end
 
 ### Tracing
