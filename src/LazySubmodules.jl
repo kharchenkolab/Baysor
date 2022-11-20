dede0640ed9eeb107d3f826b0309274f6fc19361
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2022-11-20 23:42:42 +0400

    Fixed LazySubmodules
    Apparently it still doesn't work as none of the needed functions are
    compiled into the calling modules

--- src/LazySubmodules.jl
+++ src/LazySubmodules.jl
@@ -1,49 +1,17 @@
-# import Random.randstring
-
-# mutable struct LazySubmodule
-#     _include_path::String
-#     _module::Union{Module, Nothing}
-# end
-
-# # Later it should check for environment variable to decide whether to import immediately
-# # lazy_submodule(include_path::String) = LazySubmodule(include_path, nothing)
-# function lazy_submodule(include_path::String)
-#     parent = Base.source_path(nothing)
-#     include_path = (parent === nothing) ? abspath(include_path) : normpath(joinpath(dirname(parent), include_path))
-#     return LazySubmodule(include_path, nothing)
-# end
-
-# function rename_mod(expr::Expr, new_name::Symbol)
-#     if expr.head == :module
-#         expr.args[2] = new_name
-#     end
-#     return expr
-# end
-
-# function Base.getproperty(m::LazySubmodule, s::Symbol)
-#     if s in (:_include_path, :_module)
-#         return getfield(m, s)
-#     end
-
-#     if m._module === nothing
-#         mod_name = Symbol("mod_$(randstring(16))")
-#         # m._module = include(e -> rename_mod(e, mod_name), m._include_path)
-#         m._module = include(m._include_path)
-#     end
-
-#     return getfield(m._module, s)
-# end
-
 module LazySubmodules
+
+# TODO: use include_dependency for Revise.jl (see https://docs.julialang.org/en/v1/base/base/)
 
 export @lazy_submodule
 
 const _LAZYMODE = Ref(true)
+const _LOAD_LOCKER = Threads.ReentrantLock()
 
 mutable struct LazySubmodule
-    _module_name::Symbol
+    _alias::Symbol
+    _module_name::Union{Symbol, Nothing}
     _include_path::String
-    _module::Union{Module, Nothing}
+    _parent_module::Module
 end
 
 macro lazy_submodule(ex)
@@ -54,18 +22,45 @@ macro lazy_submodule(ex)
     if !_LAZYMODE[]
         Core.eval(__module__, :($(name) = include($(path))))
     else
-        s = LazySubmodule(name, String(path), __module__)
+        s = LazySubmodule(name, nothing, String(path), __module__)
         Core.eval(__module__, :($(name) = $s))
     end
 end
 
 function Base.getproperty(m::LazySubmodule, s::Symbol)
-    if s in (:_include_path, :_module, :_module_name)
+    if s in (:_alias, :_module_name, :_include_path, :_parent_module)
         return getfield(m, s)
     end
 
-    mod = Core.eval(m._module, :($(m._module_name) = include($(m._include_path))))
-    return getfield(mod, s)
+    f = nothing
+    lock(_LOAD_LOCKER) do
+        if m._module_name === nothing
+            # Ideally, this function should be called only once per module
+            # However in case of multi-threading or for keyword argument functons it can be called multiple times
+            # See # https://discourse.julialang.org/t/how-to-properly-implement-lazy-compilation-of-submodules-problem-with-invokelatest-for-kwarg-functions/90459
+            mod = Core.eval(m._parent_module, :(include($(m._include_path))))
+            m._module_name = Symbol(split("$(mod)", ".")[end])
+        else
+            mod = getfield(m._parent_module, m._module_name)
+        end
+
+        f = getfield(mod, s)
+    end
+
+    if !(f isa Function)
+        return f
+    end
+
+    # invokelatest solves issues with world age and missing functions
+    return (args...; kwargs...) -> Base.invokelatest(f, args...; kwargs...)
+    # return (args...; kwargs...) -> begin
+        # res = Base.invokelatest(f, args...; kwargs...)
+        # Substitude the LazySubmodule with an actual compiled module
+        # lock(_LOAD_LOCKER) do
+        #     Core.eval(m._parent_module, :($(m._alias) = $(m._module_name)))
+        # end
+        # return res
+    # end
 end
 
 function __init__()
