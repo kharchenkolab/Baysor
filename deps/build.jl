b8120e83e46bff6cfde70d2d41dff9d7714cac12
Viktor Petukhov <viktor.petukhov+gh@pm.me>
2022-12-12 22:38:27 +0400

    Pull dependencies in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,10 @@ ENV["LazyModules_lazyload"] = "false"
 
 using Baysor
 using Comonicon: Builder
+using Pkg
 
+Pkg.activate(pkgdir(Baysor))
+Pkg.instantiate() # Comonicon doesn't pull non-registered dependencies, such as VegaLite.jl
+
+# delete!(ENV, "SHELL") # Disable autocompletion installation, as it doesn't work with sub-modules
 Builder.command_main(Baysor)
