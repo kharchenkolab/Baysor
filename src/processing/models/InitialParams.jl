5ab9b8236f6d28c88e31046e8c07cea81c99a7dc
viktor_petukhov <viktor.s.petuhov@ya.ru>
2023-08-29 09:07:18 -0400

    Removed unused parameter in InitialParams

--- src/processing/models/InitialParams.jl
+++ src/processing/models/InitialParams.jl
@@ -3,7 +3,7 @@ struct InitialParams{L}
     covs::Array{CovMat{L},1};
     assignment::Vector{Int64};
 
-    InitialParams(centers::Matrix{<:Real}, covs::Float64, assignment::Vector{Int64}) where N =
+    InitialParams(centers::Matrix{<:Real}, covs::Float64, assignment::Vector{Int64}) =
         new{N}(deepcopy(centers), [CovMat{N}(diagm(0 => (ones(N) .* covs))) for i in 1:size(centers, 1)], deepcopy(assignment))
 
     InitialParams(centers::Matrix{<:Real}, covs::Array{<:CovMat{N}, 1}, assignment::Array{Int64,1}) where N =
